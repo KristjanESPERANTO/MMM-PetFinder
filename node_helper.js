0155a0d6af4217c19d3b6a30d2a4688930fdc895
PhantomInferno <harry@surmafamily.com>
2023-02-22 16:36:55 -0800

    Updates to node_helper.js
    Updated the node helper to include a helper for necessary OAuth operations

--- node_helper.js
+++ node_helper.js
@@ -2,8 +2,10 @@
  * Module: MMM-PetFinder
  *
  * By Mykle1
- *
+ * Updated By PhantomOffKanagawa
+ * 
  */
+
 const NodeHelper = require('node_helper');
 const request = require('request');
 
@@ -15,20 +17,44 @@ module.exports = NodeHelper.create({
         console.log("Starting node_helper for: " + this.name);
     },
 
-    getPetFinder: function(url) {
+    getPetFinder: function(json) {
         request({
-            url: url,
+            headers: {
+                Authorization: 'Bearer ' + json.token,
+            },
+            url: json.url,
             method: 'GET'
         }, (error, response, body) => {
             if (!error && response.statusCode == 200) {
-                var result = JSON.parse(body).petfinder;
-                    this.sendSocketNotification('PETFINDER_RESULT', result);
-		
+                var result = JSON.parse(body);
+                // var result = response;
+                this.sendSocketNotification('ANIMALS', result);
             }
         });
     },
 
+    getToken: function(json) {
+        request({
+            url: json.url,
+            // body: {'msg': "grant_type=client_credentials&client_id=" + json.clientID + "&client_secret=" + json.clientSecret},
+            form: {
+                grant_type: 'client_credentials',
+                client_id: json.clientID,
+                client_secret: json.clientSecret
+            },
+            method: 'POST'
+        }, (error, response, body) => {
+            // if (!error && response.statusCode == 401) {
+                var result = JSON.parse(body).access_token;
+                this.sendSocketNotification('TOKEN', result);
+            // }
+        });
+    },
+
     socketNotificationReceived: function(notification, payload) {
+        if (notification === 'TOKEN') {
+            this.getToken(payload);
+        }
         if (notification === 'GET_PETFINDER') {
             this.getPetFinder(payload);
         }
